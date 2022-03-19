087007b05d42ad78038f5cfc13b45346db5688ed
SajadRahimi1 <sajadrahimi0101@gmail.com>
2022-03-19 19:53:59 +0330

    use typeorm

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { ReportsService } from './reports.service';
+import { Report } from './report.entity';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([Report])
+  ],
   controllers: [ReportsController],
   providers: [ReportsService]
 })
