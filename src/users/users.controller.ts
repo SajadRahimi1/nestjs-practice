38d196e29d659adb89016f921b5d4d57916be46e
SajadRahimi1 <sajadrahimi0101@gmail.com>
2022-03-31 10:52:18 +0430

    change user controller for singup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,35 +1,35 @@
-import { Controller, Post, Get, Body, Param,Patch,Query,Delete,UseInterceptors,ClassSerializerInterceptor } from '@nestjs/common';
+import { Controller, Post, Get, Body, Param, Patch, Query, Delete, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
-import {UpdateUserDto} from './dto/update-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 @Controller('auth')
 export class UsersController {
-    constructor(private userService: UsersService) { };
+    constructor(private userService: UsersService, private authService: AuthService) { };
     @Post('signup')
     createUser(@Body() body: CreateUserDTO) {
-        console.log(body);
-        this.userService.createUser(body.email, body.password);
+        return this.authService.signup(body.email, body.password);
     }
 
     @UseInterceptors(ClassSerializerInterceptor)
 
     @Get("/:id")
-    findUser(@Param('id') id:string){
+    findUser(@Param('id') id: string) {
         return this.userService.findOne(parseInt(id));
     }
 
     @Get()
-    findAllUser(@Query('email') email:string){
+    findAllUser(@Query('email') email: string) {
         return this.userService.find(email);
     }
 
     @Delete()
-    removeUser(@Param('id') id:string){
+    removeUser(@Param('id') id: string) {
         return this.userService.remove(parseInt(id));
     }
 
     @Patch('/:id')
-    updateUser(@Param('id') id:string, @Body() body:UpdateUserDto){
-        return this.userService.update(parseInt(id),body);
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.userService.update(parseInt(id), body);
     }
 }
